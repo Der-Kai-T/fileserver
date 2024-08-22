87e2ca67ab92b69946a189f10edb93823865c6b5
Kai <github@kai-thater.de>
2024-08-22 14:57:02 +0200

    fix permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -14,6 +14,7 @@ class PermissionSeeder extends Seeder
             "file.create",
             "file.update",
             "file.destroy",
+            "file",
             "admin.user",
         ];
 
